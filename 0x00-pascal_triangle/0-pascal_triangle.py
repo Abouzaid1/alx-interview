7f80310bf414044890f32f397e10919ac0b7de00
Your Name <am5715073@gmail.com>
2024-07-26 02:34:43 +0300

    doc

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,14 +6,16 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+    # first element
     triangle = [[1]]
     
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
+             # last element
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
+        # Add two previous values to get current next value
         triangle.append(row)
     
     return triangle
